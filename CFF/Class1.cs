59bf9af7110408a6e0243aa6578ae818284ece76
Randall Sexton <jrandallsexton@gmail.com>
2014-10-14 10:49:26 -0400

    Initial Commit

---
+++ CFF/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFF
+{
+
+    public class Forecast
+    {
+
+    }
+
+    public class ForecastItem
+    {
+
+    }
+
+    public enum ForecastType
+    {
+        Indefinite = 0,
+        Snapshot = 1
+    }
+
+}
